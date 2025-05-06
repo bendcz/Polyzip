41689fe08fffa2de4bdd735e0e25ff2665f9b412
bendcz <210593246+bendcz@users.noreply.github.com>
2025-05-06 21:36:31 +0200

    Display program name, extract argument to select RLE algorithm.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    printf("Hello World!\n");
-    return EXIT_FAILURE;
+    printf("▗▄▄▖  ▄▄▄  █ ▄   ▄ ▄▄▄▄▄ ▄ ▄▄▄▄\n");
+    printf("▐▌ ▐▌█   █ █ █   █  ▄▄▄▀ ▄ █   █\n");
+    printf("▐▛▀▘ ▀▄▄▄▀ █  ▀▀▀█ █▄▄▄▄ █ █▄▄▄▀\n");
+    printf("▐▌         █ ▄   █       █ █     \n");
+    printf("              ▀▀▀          ▀    \n");
+    printf("This program compresses files using RLE encoding.\n");
+
+    if (argc < 3)
+    {
+        printf("\n/!\\ Error during program call.\n");
+        printf("Usage: ./polyzip --rle <FILE_PATH>\n");
+        return EXIT_FAILURE;
+    }
+
+    if (strncmp(argv[1], "--rle", 5) == 0)
+    {
+
+    }
+
+    return EXIT_SUCCESS;
 }
