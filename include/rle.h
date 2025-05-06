3b71ec4b0b339c79b1e6d4fafcb69b784e170937
bendcz <210593246+bendcz@users.noreply.github.com>
2025-05-06 21:58:46 +0200

    Creation of rle.h and rle.c

---
+++ include/rle.h
@@ -0,0 +1,9 @@
+#ifndef RLE_H
+#define RLE_H
+
+#include <string.h>
+
+void compress_rle(const char *filePath);
+void decompress_rle(const char *filePath);
+
+#endif
---
+++ src/rle.c
@@ -0,0 +1,11 @@
+#include "../include/rle.h"
+
+void compress_rle(const char *filePath)
+{
+
+}
+
+void decompress_rle(const char *filePath)
+{
+    
+}
